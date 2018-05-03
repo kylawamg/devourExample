68ffbf4c224256831834a6423d259368d7c5b185
Alex <alex@MacBook-Pro-de-Alex.local>
2018-05-03 14:50:58 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,50 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import JsonApi from 'devour-client'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
-  }
-}
+  },
+  mounted() {
+      const jsonApi = new JsonApi({apiUrl: 'https://live-contentacms.pantheonsite.io/api'});
+/* Get Normal trae los datos en un objeto Arr de objetos planos.
+     jsonApi.define('recipe', {
+      title: ''
+  });
+   jsonApi.findAll('recipes')
+       .then(res => {
+       console.log(res);
+  });*/
+// Get con include, hay que definir primero ambos modelos y luego hacer la peticion. El included viene en cada elemento como una key mas.
+      jsonApi.define('recipe', {
+          title: '',
+          difficulty: '',
+          category: {
+              jsonApi: 'hasOne',
+              type: 'categories'
+          }
+      })
+
+      jsonApi.define('category', {
+          name: ''
+      });
+jsonApi.findAll('recipes', {include: 'category'})
+          .then((res) => {
+         console.log(res);
+      });
+      jsonApi.create('recipe', {
+          title: 'hello',
+          difficulty: 'easy',
+      }).then((res) => {
+          console.log(res);
+      });
+      console.log(jsonApi);
+
+  },
+};
 </script>
 
 <style>
